1664ab8257184e24e41590ec65726cd58976d581
saroj <sarojpanrui@gmail.com>
2025-03-31 18:38:35 +0530

    added backend

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -10,7 +10,7 @@ function Course() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://book-store-app-one-ecru.vercel.app/book");
         console.log(res.data);
         setBook(res.data);
       } catch (error) {
--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -13,7 +13,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://book-store-app-one-ecru.vercel.app/book");
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
