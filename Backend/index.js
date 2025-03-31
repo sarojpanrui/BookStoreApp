90d9a335995a92c1a7d167f1c4660b4b32e16675
saroj <sarojpanrui@gmail.com>
2025-03-31 19:13:53 +0530

    fixed the frontend

--- Backend/index.js
+++ Backend/index.js
@@ -2,35 +2,38 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
+dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
-
-dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const URI = process.env.MongoDBURI;
 
-// connect to mongoDB
-try {
-    mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to mongoDB");
-} catch (error) {
-    console.log("Error: ", error);
-}
+// Connect to MongoDB
+mongoose
+    .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.log("Error:", error));
 
-// defining routes
+// Define routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
+// Serve React Frontend
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "Frontend", "dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
